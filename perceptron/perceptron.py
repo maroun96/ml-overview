948f8e6933af98b58f8be97a680e2da9b9bd2e64
karl maroun <karlmaroun@karls-MacBook-Air.local>
2024-09-30 23:13:35 +0200

    implemented fit method

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -12,6 +12,21 @@ class Perceptron:
         self.b_ = np.float64(0.)
         self.errors_ = []
 
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta*(target - self.predict(xi))
+                self.w_ += update*xi
+                self.b_ += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+    
+    def net_input(self, X):
+        return np.dot(X, self.w_) + self.b_
+    
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+
 
 if __name__ == "__main__":
     percp = Perceptron()
